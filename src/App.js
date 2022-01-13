1d4e51440c5ef3919104c2acae886c59454bff1f
jomadoye <omadoyejed@gmail.com>
2022-01-13 15:32:53 +0100

    Add card details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+import Card from "./Card";
 import "./App.css";
 
 const Wrapper = styled.div`
@@ -10,88 +11,24 @@ const Wrapper = styled.div`
   align-items: flex-end;
 `;
 
-const CardContainer = styled.div`
-  width: 327px;
-  height: 203px;
-  left: 40px;
-  top: 40px;
-
-  background: linear-gradient(259.37deg, #162288 2.76%, #3a49b6 68.13%);
-  border-radius: 10px;
-`;
-
-const SwitchContainer = styled.div`
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 60px;
-    height: 34px;
-  }
-
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-  }
-
-  .slider:before {
-    position: absolute;
-    content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-  }
-
-  input:checked + .slider {
-    background-color: #3A49B6;
-  }
-
-  input:focus + .slider {
-    box-shadow: 0 0 1px #3A49B6;
-  }
-
-  input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
-  }
-
-  /* Rounded sliders */
-  .slider.round {
-    border-radius: 34px;
-  }
-
-  .slider.round:before {
-    border-radius: 50%;
-  }
-`;
+const SwitchContainer = styled.div``;
 
 function App() {
+  const cardDetails = {
+    name: "BRUCE WAYNE",
+    first12Nums: "1234 1234 1234",
+    last4Nums: "1234",
+    cvv: "123",
+    expiryDate: "09/12",
+  };
   return (
     <div className="App">
       <Wrapper>
-        <CardContainer />
+        <Card cardDetails={cardDetails} />
 
         <SwitchContainer>
           <label class="switch">
             <input type="checkbox" checked />
-            <span class="slider round"></span>
           </label>
         </SwitchContainer>
       </Wrapper>
---
+++ src/Card.js
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+import "./App.css";
+
+const Wrapper = styled.div`
+  width: 327px;
+  height: 203px;
+  left: 40px;
+  top: 40px;
+  padding: 28px 24px;
+
+  background: linear-gradient(259.37deg, #162288 2.76%, #3a49b6 68.13%);
+  border-radius: 10px;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const Name = styled.h1`
+  font-size: 16px;
+  line-height: 12px;
+  color: #ffffff;
+  text-shadow: 0.954545px 0.954545px 0px rgba(0, 0, 0, 0.25);
+  font-style: normal;
+  font-weight: normal;
+`;
+
+const CardNnumber = styled.h1`
+  font-size: 20px;
+  line-height: 18px;
+  color: #ffffff;
+  text-shadow: 0.954545px 0.954545px 0px rgba(0, 0, 0, 0.25);
+
+  font-style: normal;
+  font-weight: normal;
+`;
+
+const ExpiryAndCvvContainer = styled.div`
+  display: flex;
+`;
+const ExpiryContainer = styled.div`
+  width: 100px;
+`;
+
+const ExpiryAndCvvText = styled.h1`
+  font-size: 10px;
+  line-height: 10px;
+  color: #ffffff;
+  text-shadow: 0.954545px 0.954545px 0px rgba(0, 0, 0, 0.25);
+
+  font-style: normal;
+  font-weight: normal;
+  margin-bottom: 0px;
+`;
+
+const ExpiryAndCvvValue = styled.h1`
+  font-size: 14px;
+  line-height: 12px;
+  color: #ffffff;
+  text-shadow: 0.954545px 0.954545px 0px rgba(0, 0, 0, 0.25);
+
+  font-style: normal;
+  font-weight: bold;
+  margin-top: 2px;
+`;
+
+function Card({ cardDetails }) {
+  const { name, first12Nums, last4Nums, cvv, expiryDate } = cardDetails;
+  return (
+    <Wrapper>
+      <Name>{name}</Name>
+      <CardNnumber>{`${first12Nums} ${last4Nums}`}</CardNnumber>
+
+      <ExpiryAndCvvContainer>
+        <ExpiryContainer>
+          <ExpiryAndCvvText>VALID THRU</ExpiryAndCvvText>
+          <ExpiryAndCvvValue>{expiryDate}</ExpiryAndCvvValue>
+        </ExpiryContainer>
+
+        <ExpiryContainer>
+          <ExpiryAndCvvText>CVV</ExpiryAndCvvText>
+          <ExpiryAndCvvValue>{cvv}</ExpiryAndCvvValue>
+        </ExpiryContainer>
+      </ExpiryAndCvvContainer>
+    </Wrapper>
+  );
+}
+
+export default Card;
